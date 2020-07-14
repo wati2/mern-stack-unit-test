02eed2b46bcf6633c6661c61c254d7e9948b38cd
wati2 <yt.wati@gmail.com>
2020-07-14 20:54:47 +0900

    add

--- routes.js
+++ routes.js
@@ -4,10 +4,12 @@ const birds = require('./router/birds')
 const root = require('./router/root')
 const test = require('./router/test3')
 const test4 = require('./router/test4')
+const myError = require('./router/error')
 
 app.use('/', root)
 app.use('/birds',birds)
 app.use('/test3',test)
 app.use('/test4',test4)
+app.use('/',myError)
 
 module.exports = app
